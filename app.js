f6bac86a5206ceed1d25ca832d75d4bf0a945e6a
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-01-22 10:05:05 +0545

    setup

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const products_routes = require("./routes/products")
+
 app.get("/", (req, res) =>{
     res.send("Hi , I am a man.");
 });
+
+
+//middleware or to set router
+app.use("/api/products", products_routes);
 
 const start = ()=>{
     try{
